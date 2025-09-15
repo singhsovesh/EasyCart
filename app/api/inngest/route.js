00ef31ae83ce32c55a57ee569c047f64a4143243
Sovesh Singh <singhsovesh25@gmail.com>
2025-09-15 19:40:45 +0530

    error resolve

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,17 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
+import {
+  syncUserCreation,
+  syncUserUpdation,
+  syncUserDeletion,
+} from "../../../inngest/functions"; // <-- import your functions here
 
-// Create an API that serves zero functions
+// Create an API that serves your functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-       syncUserCreation,
-       syncUserUpdation,
-       syncUserDeletion
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion,
   ],
 });
